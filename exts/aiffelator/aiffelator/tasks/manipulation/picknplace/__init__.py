041ac016ed5d0c39b63cb24723f2d13f8c5f9e25
zave7 <if.zave@gmail.com>
2024-10-15 15:41:23 +0900

    add rl-ppo config files

--- exts/aiffelator/aiffelator/tasks/manipulation/picknplace/__init__.py
+++ exts/aiffelator/aiffelator/tasks/manipulation/picknplace/__init__.py
@@ -9,6 +9,9 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": SingleObjectPickAndPlaceEnvCfg,
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:SingleTablePickAndPlacePPORunnerCfg"
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:SingleTablePickAndPlacePPORunnerCfg",
+        "sb3_cfg_entry_point": f"{agents.__name__}:sb3_ppo_cfg.yaml",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml"
     },
 )
