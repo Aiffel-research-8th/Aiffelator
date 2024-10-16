8e82c7a8a3f91d4ff06ac32742d3cb4ed2846e04
zave7 <if.zave@gmail.com>
2024-10-16 14:04:29 +0900

    modified gym registration

--- exts/aiffelator/aiffelator/tasks/manipulation/picknplace/__init__.py
+++ exts/aiffelator/aiffelator/tasks/manipulation/picknplace/__init__.py
@@ -22,7 +22,7 @@ gym.register(
     disable_env_checker=True,
     kwargs={
         "env_cfg_entry_point": SingleObjectPickAndPlaceEnvCfg_PLAY,
-        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:LiftCubePPORunnerCfg",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:SingleTablePickAndPlacePPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
         "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
     }
